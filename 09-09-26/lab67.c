a50aa51f244616e163e7a3fbae738eea59e38e8b
shanmukhabhavajna <murrushanmukhabhavajna@gmail.com>
2026-09-09 16:04:41 +0530

    add lab 67.c

---
+++ 09-09-26/lab67.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){char str[100];char ch,chh;
+	printf("enter a word:");
+	scanf("%s",&str);
+	printf("enter a character:");
+	scanf(" %c",&ch);
+	printf("enter a character:");
+           scanf(" %c",&chh);
+	   for(int i=0;str[i]!='\0';i++){if(str[i]==ch){str[i]=chh;}}
+	   printf("%s",str);
+	   return 0;}
